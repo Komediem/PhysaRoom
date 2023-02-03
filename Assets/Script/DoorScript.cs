fb4d87a14b3821e26880018acd559cd97cc3f15f
Komediem <gbussola@artfx.fr>
2023-02-03 23:53:26 +0100

    Door Avancement

--- Assets/Script/DoorScript.cs
+++ Assets/Script/DoorScript.cs
@@ -5,27 +5,16 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public Animator doorAnimator;
-    public bool Opened = false;
 
     public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            //Debug.Log("Bing Chilling");
+            Debug.Log("bing Chilling");
 
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
-                if (Opened == false)
-                {
-                    doorAnimator.SetBool("IsOpening", true);
-                    Opened = true;
-                }
-
-                else if (Opened == true)
-                {
-                    doorAnimator.SetBool("IsOpening", false);
-                    Opened = false;
-                }
+                doorAnimator.SetBool("IsOpening", true);
             }
         }
     }
