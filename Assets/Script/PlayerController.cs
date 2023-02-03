7fc6ce4f39d8204dd87dd6b5c46b04b291ed8a84
Komediem <gbussola@artfx.fr>
2023-02-03 16:02:07 +0100

    Player Mouvement
    [refactor] Movement Script
    [add] Colliders

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
+    Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    Rigidbody2D rb;
+    Vector2 position = new Vector2(0f, 0f);
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            rb.MovePosition(position);
+        }
     }
 }
