f5547d5d7d8b592707b737e3d9c731590af08d7b
Komediem <gbussola@artfx.fr>
2023-02-03 15:25:24 +0100

    MainMenu Fonctionnel

--- Assets/Script/MainMenuScript.cs
+++ Assets/Script/MainMenuScript.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
     public void PlayGame()
     {
+        SceneManager.LoadScene("LevelOne");
+    }
 
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
