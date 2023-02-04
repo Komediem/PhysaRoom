9b6ecd608504d69f565ca5b2b1290c546d251cb9
Komediem <gbussola@artfx.fr>
2023-02-04 17:00:41 +0100

    Door and Interruptor fonctionable

--- Assets/Script/DoorScript.cs
+++ Assets/Script/DoorScript.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    //public Animator doorAnimator;
+    public Animator doorAnimator;
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                doorAnimator.SetBool("IsOpening", true);
+            }
+        }
+    }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,7 +9,6 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 0.05f;
     Rigidbody2D rb;
     Vector2 position = new Vector2(0f, 0f);
-    public Animator doorAnimator;
 
     private void Start()
     {
@@ -28,17 +27,6 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.Mouse0))
         {
             rb.MovePosition(position);
-        }
-    }
-
-    public void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("InterruptorDoor"))
-        {
-            if (Input.GetKeyDown(KeyCode.Mouse1))
-            {
-                doorAnimator.SetBool("IsOpening", true);
-            }
         }
     }
 
