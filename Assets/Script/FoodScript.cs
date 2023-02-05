36950f8214dae7f0288da8ccbde9d5e3e5a268bc
Komediem <gbussola@artfx.fr>
2023-02-05 17:14:24 +0100

    Pause Button Desactivation

--- Assets/Script/FoodScript.cs
+++ Assets/Script/FoodScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class FoodScript : MonoBehaviour
 {
     public GameObject experienceText;
+    public GameObject pauseButton;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,6 +18,7 @@ public class FoodScript : MonoBehaviour
 
     public IEnumerator NextLevel()
     {
+        pauseButton.SetActive(false);
         experienceText.SetActive(true);
         yield return new WaitForSeconds(2);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
